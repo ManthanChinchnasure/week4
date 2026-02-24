260c893d0d13ce00f81c2e623d867203e47918b5
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-02-25 00:02:08 +0530

    class funcation

---
+++ class.js
@@ -0,0 +1,44 @@
+class Rectangle {
+    constructor(width,height,color){
+        this.width=width;
+        this.height=height;
+        this.color=color;
+ 
+    }
+
+    area(){
+        const area = this.width   *  this.height;
+        return area;
+    }
+
+
+  // this is written by without see the video..
+
+}
+
+const rect = new Rectangle(4, 4)
+const area = rect.area();                   // this three line i am confusion where it initalize a object..
+console.log(area)
+
+
+// class Rectangle {
+//    constructor(width, height, color) {
+// 	    this.width = width;
+// 	    this.height = height;
+// 	    this.color = color; 
+//    }
+   
+//    area() {
+// 	   const area = this.width * this.height;
+// 		 return area;
+//    }
+   
+//    paint() {
+// 			console.log(`Painting with color ${this.color}`);
+//    }
+   
+// }
+
+// const rect = new Rectangle(2, 4)
+// const area = rect.area();
+// console.log(area)
