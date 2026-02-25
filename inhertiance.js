9eb57567429cb1fe33bf325e37151e02f84f444e
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-02-25 22:59:36 +0530

    inhert

--- inhertiance.js
+++ inhertiance.js
@@ -5,10 +5,18 @@ class Rectangle {
         this.color=color;
  
     }
-
-    area(){
+ 
+    area() {
         const area = this.width   *  this.height;
         return area;
+    }
+
+    perimeter () {
+       return this.width * this.height;
+    }
+
+    color() {
+        console.log("paintaing with color " + this.color);
     }
 
 
@@ -16,7 +24,24 @@ class Rectangle {
 
 }
 
-const rect = new Rectangle(4, 4)
-const area = rect.area();                
-console.log(area)
+
+class circle {
+    constructor(radius){
+        this.radius=radius;
+    }
+
+    area() {
+        return 3.14 *this.radius * this.radius;
+    }
+
+    parimeter () {
+        return 2 * 3.14 *(this.radius)
+    }
+
+    paint () {
+         console.log("paintaing with color " + this.color);
+    }
+
+    
+}
 
