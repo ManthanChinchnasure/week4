a9e6167ebcc827f74c31c790a54ce74d87d89b4d
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-03-15 00:12:55 +0530

    it do diffcult to set perious rhythem again

--- review/s.js
+++ review/s.js
@@ -9,3 +9,31 @@ console.log(contents);
 
 
 */
+
+
+/*  === it is an asynchronous code..
+
+const fs = require("fs");
+
+function ReadFileload(err, c){
+    console.log(c);
+}
+
+fs.readFile("b.txt", "utf-8", ReadFileload);
+
+for (i=0 ; i< 70000000 ; i++){
+    
+}
+
+*/
+
+/*  == set Timeout funcation where how the function callback it self it deffine inn it 
+function callback(){
+    console.log("hi there");
+
+}
+
+
+setTimeout( callback,5*1000 );
+
+*/
