9ab82d5495526cf169bfdf225f3bafc44aaee8df
ManthanChinchansure <manthanchinchansure68@gmail.com>
2026-03-14 23:49:31 +0530

    at few day  everything is flesh

---
+++ review/s.js
@@ -0,0 +1,11 @@
+
+
+/* ==  it is a synchronous code where it run excute line by line 
+
+const fs = require("fs");
+
+let contents = fs.readFileSync("b.txt", "utf-8");
+console.log(contents);
+
+
+*/
